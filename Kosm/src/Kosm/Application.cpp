c6df57bc484c21f78d644d7a15c605aeb7c7cb25
Ryan Valenza <ryvalenza@gmail.com>
2024-05-21 10:54:02 -0400

    add: window eventing

--- Kosm/src/Kosm/Application.cpp
+++ Kosm/src/Kosm/Application.cpp
@@ -2,22 +2,33 @@
 #include "Application.h"
 
 #include "Kosm/Events/ApplicationEvent.h"
+#include <GLFW/glfw3.h>
 
 namespace Kosm {
+
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		KOSM_CORE_INFO(e.ToString());
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
 	};
--- Kosm/src/Kosm/Application.h
+++ Kosm/src/Kosm/Application.h
@@ -13,6 +13,7 @@ namespace Kosm {
 
 		void Run();
 
+		void OnEvent(Event& e);
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
